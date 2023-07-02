e24d122c1425cb886464aa1db2799289d8eae837
Janumala Akhilendra <82641474+JanumalaAkhilendra@users.noreply.github.com>
2023-07-02 12:30:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ class Chatbox {
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
 
-        fetch('http://akhilendra.pythonanywhere.com/predict', {
+        fetch('https://ai-chat-bot-4.onrender.com/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
